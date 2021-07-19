a72bed64aa690d8ecba6a8c1b124efe601f05e84
niusmallnan <niusmallnan@gmail.com>
2021-07-19 20:11:35 +0800

    Update  run-aarch64-iso.sh

--- tools/run-aarch64-iso.sh
+++ tools/run-aarch64-iso.sh
@@ -2,26 +2,33 @@
 # Install distro from an ISO onto a virtual disk image
 # . runqemu-iso <distro>.iso
 #
-# QEMU_EFI.img:
-#   wget https://releases.linaro.org/components/kernel/uefi-linaro/latest/release/qemu64/QEMU_EFI.img.gz
-#   gunzip QEMU_EFI.img.gz
+# openSUSE 15.3
+# please disable the firewalld with yast
+#   zypper in qemu qemu-arch
+#
+
 
 iso=$1
 os=${iso%.iso}
-size=8G
+size=5G
 
 disk_img=$os.qcow2
 efi_img=$os.efi
 
 qemu-img create -f qcow2 $disk_img ${size}
 qemu-img create -f qcow2 $efi_img 64M
-
+# -nographic
+# -vnc :1 -monitor stdio
+# -bios /usr/share/qemu/qemu-uefi-aarch64.bin
 exec qemu-system-aarch64 \
-    -cpu cortex-a53 -M virt -m 4096 -nographic -smp 2 -boot d \
-    -drive if=pflash,format=raw,file=QEMU_EFI.img \
-    -drive if=pflash,format=qcow2,file=${efi_img} \
+    -cpu cortex-a57 -M virt -m 4096 -smp 2 -boot d \
+    -bios /usr/share/qemu/qemu-uefi-aarch64.bin \
     -device virtio-scsi-device \
     -device scsi-cd,drive=cdrom \
     -device virtio-blk-device,drive=hd0 \
+    -device virtio-rng-pci \
     -drive file=${iso},id=cdrom,media=cdrom,if=none \
-    -drive file=${disk_img},id=hd0,if=none
+    -drive file=${disk_img},id=hd0,if=none \
+    -rtc base=utc,clock=host \
+    -monitor stdio \
+    -vnc 0.0.0.0:1
